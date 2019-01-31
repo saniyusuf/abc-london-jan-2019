d8648dab83fc9c2847aef94a3c8464a839b588d6
Sani Yusuf <saniyusuf@gmail.com>
2019-01-31 11:24:33 +0000

    step 202

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", loadChildren: './dashboard/dashboard.module#DashboardModule'},
-  // { path: "", redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: "", redirectTo: 'dashboard', pathMatch: 'full' },
   { path: "dashboard", loadChildren: './dashboard/dashboard.module#DashboardModule'}
 ];
 
